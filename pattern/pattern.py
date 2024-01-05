4268d2401d58bf2c7047338aac82fa898dbedffc
Paulos Tesfazghi <71388869+pth2020@users.noreply.github.com>
2024-01-05 17:22:22 +0000

    Update pattern.py

--- pattern/pattern.py
+++ pattern/pattern.py
@@ -1,1 +1,30 @@
+#This program prints star(asterisk) patters using if-else and a single for loop
+
+'''
+The pattern that will be produced when the code below runs is:
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+'''
+
+#intialising variables
+stars = "*"
+index = 9
+
+for i in range(0,index):
+        if i < 5:
+                print(stars)
+                stars += "*" #one asterisk at a time is concatenated to the string 'stars'
+        else:
+                print(stars[0:index-i]) #prints the string 'stars' by slicing one asterisk at a time
+                
+        
 
